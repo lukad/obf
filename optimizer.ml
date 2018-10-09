72ce29c31ca0a2b3841f0d61b551368ecebd71e8
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-10-09 22:57:35 +0200

    fix: don't omit empty loops unconditionally

--- optimizer.ml
+++ optimizer.ml
@@ -6,17 +6,17 @@ let rec opt = function
   | Add 0  :: rest -> opt rest
   | Move 0 :: rest -> opt rest
 
-  | Add a  :: Add b  :: rest -> Add (a + b) :: opt rest
+  | Add a  :: Add b  :: rest -> Add (a + b)  :: opt rest
   | Move a :: Move b :: rest -> Move (a + b) :: opt rest
 
-  | Loop []         :: rest ->                    opt rest
-  | Loop [(Add -1)] :: rest -> Set 0           :: opt rest
-  | Loop body       :: rest -> Loop (opt body) :: opt rest
+  | Loop [(Add -1)]         :: rest -> Set 0           :: opt rest
+  | Loop body :: Loop _body :: rest -> Loop (opt body) :: opt rest
+  | Loop body               :: rest -> Loop (opt body) :: opt rest
 
   | Set a :: Add b  :: rest -> Set (a + b) :: opt rest
-  | Add _ :: Set a  :: rest -> Set a       :: opt rest
   | Set _ :: Set a  :: rest -> Set a       :: opt rest
   | Set 0 :: Loop _ :: rest -> Set 0       :: opt rest
+  | Add _ :: Set a  :: rest -> Set a       :: opt rest
 
   | ins :: rest -> ins :: (opt rest)
 
