9ad513387a25d7baf96a8aeb1afed894494ed0ef
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-10-10 23:14:02 +0200

    refactor: rename whitespace to comment

--- reader.ml
+++ reader.ml
@@ -1,7 +1,7 @@
 open Angstrom
 
-let is_whitespace c = not (List.mem c ['+'; '-'; '>'; '<'; ','; '.'; '['; ']'])
-let ws = many (satisfy is_whitespace)
+let is_comment c = not (List.mem c ['+'; '-'; '>'; '<'; ','; '.'; '['; ']'])
+let comment = many (satisfy is_comment)
 
 let chars c f = many1 (char c) >>= f
 
@@ -12,10 +12,10 @@ let right = chars '<' (fun s -> return (Ast.Move ~-(List.length s)))
 let read = char ',' *> return Ast.Read
 let write = char '.' *> return Ast.Write
 
-let simple = (plus <|> minus <|> left <|> right <|> read <|> write) <* ws
-let loop program = (char '[' *> program <* char ']') <* ws
+let simple = (plus <|> minus <|> left <|> right <|> read <|> write) <* comment
+let loop program = (char '[' *> program <* char ']') <* comment
                    >>= fun body -> return (Ast.Loop body)
 
-let program = fix (fun program -> ws *> many (simple <|> loop program)) <* end_of_input
+let program = fix (fun program -> comment *> many (simple <|> loop program)) <* end_of_input
 
 let read_string = parse_string program
